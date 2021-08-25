a4a84d3a1bd44acba440e9a691e624aa7394c271
The Clown <77796853+TR011F4C3@users.noreply.github.com>
2021-08-25 09:47:03 +0100

    Update PλNDA.lua

--- PλNDA.lua
+++ PλNDA.lua
@@ -97,6 +97,6 @@ end)
 
 local tab = win:Tab("Credits", "http://www.roblox.com/asset/?id=6023426915")
 tab:Label("The Clown#0001 - Main Creator")
-tab:Label("Pog#5555 - Alpha/Beta Tester")
+tab:Label("pog#5555 - Alpha/Beta Tester")
 tab:Label("epik#0905 - Alpha/Beta Tester")
 tab:Label("Pandq#2169 - Error 404/Sleeping through Script Development")
